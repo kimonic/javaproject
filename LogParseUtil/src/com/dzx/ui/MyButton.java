c9d88b053514b9e9879c3655c7c459ca061b519b
dingzhixin.ex <dingzhixin.ex@hx-partners.com>
2020-04-24 13:31:01 +0800

    202004241330  添加自定义button

---
+++ LogParseUtil/src/com/dzx/ui/MyButton.java
@@ -0,0 +1,46 @@
+package com.dzx.ui;
+
+import javax.swing.*;
+
+/**
+ * 带点击id的button
+ */
+public class MyButton extends JButton {
+    /**
+     * 用于判断是哪个按钮点击的id
+     */
+    private int mClickId;
+
+    public MyButton() {
+    }
+
+    public MyButton(Icon icon) {
+        super(icon);
+    }
+
+    public MyButton(String text) {
+        super(text);
+    }
+
+    public MyButton(Action a) {
+        super(a);
+    }
+
+    public MyButton(String text, Icon icon) {
+        super(text, icon);
+    }
+
+    public MyButton(String text, int mClickId) {
+        super(text);
+        this.mClickId = mClickId;
+    }
+
+
+    public int getmClickId() {
+        return mClickId;
+    }
+
+    public void setmClickId(int mClickId) {
+        this.mClickId = mClickId;
+    }
+}
